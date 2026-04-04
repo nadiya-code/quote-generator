3707f95509896de2a6e311f745a0073c23dba520
Nadiya <nadiyakola3@gmail.com>
2026-04-05 00:51:51 +0530

    added javascript for buttons and added colors in tailwindcss

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import "./input.css";
+let button=document.querySelectorAll("button");
+button.forEach((btn) =>{
+    btn.addEventListener("mouseenter", () => {
+        if(!btn.classList.contains("new-quote")&&!btn.classList.contains("save")){
+            btn.classList.add("animate-slide-up","border-ink2","text-ink3");
+        }
+        else if(btn.classList.contains("save")){
+            btn.classList.add("animate-slide-up","border-pink","text-pink");
+        }
+        else{
+            btn.classList.add("animate-slide-up");
+        }
+    });
+    btn.addEventListener("mouseleave", () => {
+        btn.classList.remove("animate-slide-up","border-ink2","border-pink","text-pink","text-ink3");
+        void btn.offsetWidth;
+    });
+});
